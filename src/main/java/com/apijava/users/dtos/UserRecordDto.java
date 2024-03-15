583cc0856812fbab8ae7bedb0474d0f20cdca28e
Namibo06 <namibo654@gmail.com>
2024-03-14 22:27:11 -0300

    feat: UserRecordDto

---
+++ src/main/java/com/apijava/users/dtos/UserRecordDto.java
@@ -0,0 +1,16 @@
+package com.apijava.users.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRecordDto(
+        @NotBlank String first_name,
+        @NotBlank String last_name,
+        @NotBlank @Email String email,
+        @NotBlank String nivel_user,
+        @NotBlank String password
+        ) {
+    /*
+    * record define uma classe como imutavel,ou seja,depois de criada não pode ser alterada
+    * */
+}
