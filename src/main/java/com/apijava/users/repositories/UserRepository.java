d70f13ef90c90c74ab4df325524fb92ba020c765
Namibo06 <namibo654@gmail.com>
2024-03-14 22:26:56 -0300

    feat: UserRepository

---
+++ src/main/java/com/apijava/users/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.apijava.users.repositories;
+
+import com.apijava.users.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    /*
+    * estender o JPARepository serve muito bem para realizar CRUD's pois já vem com muitos comandos
+    * já definidos como findAll,save,findById...
+    * mas aqui pode ser implementadas/personalizadas queries com acima uma anotação @Query
+    * */
+}
